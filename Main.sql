e502ce864e366a891ec62142c9de195189cbf4f4
rstuartSVUSD <59625626+rstuartSVUSD@users.noreply.github.com>
2020-12-17 15:49:43 -0800

    Update Main.sql
    Added Terms
    Added Student_Number from SIS
    Calculates GPA

--- Main.sql
+++ Main.sql
@@ -28,20 +28,54 @@ grades AS (SELECT --Selects storedgrades from last 4 year termids
                     storedgrades sg
                 WHERE
                     sg.termid IN (SELECT rel_terms FROM relTerms)
-) 
-                
+),
+gpaTerms AS (SELECT 'Semesters' AS term_type, 'S1' AS Storecode FROM dual
+             UNION
+             SELECT 'Semesters' AS term_type, 'S2' AS Storecode FROM dual
+             UNION
+             SELECT 'Trimesters' AS term_type, 'T1' AS Storecode FROM dual
+             UNION
+             SELECT 'Trimesters' AS term_type, 'T2' AS Storecode FROM dual
+             UNION
+             SELECT 'Trimesters' AS term_type, 'T3' AS Storecode FROM dual)
 
-
-SELECT 
-    pse.studentid
-    ,(SELECT name FROM terms WHERE terms.yearid = pse.yearid AND terms.isyearrec = 1 AND terms.schoolid = 6052260) AS School_Year
-    ,pse.schoolname
-    ,(CASE
-        WHEN pse.grade_level < 9 THEN 'Trimesters'
-        ELSE 'Semesters'
-        END) AS GPA_Calc_Method
-    --FIX ME: Calculate GPA for (S1 and S2) or (T1, T2 and T3) depending on pse grade level
-FROM PSRW_SchoolEnrollment  pse
-WHERE
-    pse.grade_level > 5 --No grades for elementary students
-    AND pse.yearid IN (SELECT SUBSTR(rel_terms, 1, 2) FROM relTerms) --Students from the past 4 years;
+SELECT
+    e.studentid
+    ,stu.student_number
+    ,e.school_year
+    ,e.yearid
+    ,e.schoolname
+    ,e.schoolid
+    ,e.gpa_term_method
+    ,gpaterms.storecode
+    ,(SELECT 
+            ROUND(
+                SUM(DECODE(grade,'A+',4,'A',4,'A-',4,'B+',3,'B',3,'B-',3,'C+',2,'C',2,'C-',2,'D+',1,'D',1,'D-',1,'F',0))
+                / GREATEST(SUM(DECODE(grade,'A+',1,'A',1,'A-',1,'B+',1,'B',1,'B-',1,'C+',1,'C',1,'C-',1,'D+',1,'D',1,'D-',1,'F',1)),0.001)
+            ,2)
+      FROM storedgrades sg 
+      WHERE 
+            sg.studentid = e.studentid 
+            AND sg.schoolid = e.schoolid 
+            AND sg.termid = (e.yearid || '00') 
+            AND sg.storecode = gpaterms.storecode 
+            AND sg.excludefromgpa = 0) AS GPA
+FROM 
+    (SELECT 
+        pse.studentid
+        ,(SELECT name FROM terms WHERE terms.yearid = pse.yearid AND terms.isyearrec = 1 AND terms.schoolid = 6052260) AS School_Year
+        ,pse.yearid
+        ,pse.schoolname
+        ,pse.schoolid
+        ,(CASE
+            WHEN pse.grade_level < 9 THEN 'Trimesters'
+            ELSE 'Semesters'
+            END) AS GPA_Term_Method
+        --FIX ME: Calculate GPA for (S1 and S2) or (T1, T2 and T3) depending on pse grade level
+    FROM PSRW_SchoolEnrollment  pse
+    WHERE
+        pse.grade_level > 5 --No grades for elementary students
+        AND pse.yearid IN (SELECT SUBSTR(rel_terms, 1, 2) FROM relTerms) --Students from the past 4 years
+    ) e
+JOIN gpaTerms ON gpaTerms.term_type = e.GPA_term_Method
+JOIN students stu ON stu.id = e.studentid
